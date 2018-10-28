72b38997aa495c6d2d9dbe18c715d9ad1446347c
Jacek Janczura <janczura.jacek@gmail.com>
2018-10-28 19:45:06 +0100

    To throw custom exceptions connected with atm account balance, class BalanceException has been created

---
+++ src/main/java/exceptions/BalanceException.java
@@ -0,0 +1,20 @@
+package exceptions;
+
+public class BalanceException extends Exception {
+
+    public BalanceException () {
+
+    }
+
+    public BalanceException (String message) {
+        super (message);
+    }
+
+    public BalanceException (Throwable cause) {
+        super (cause);
+    }
+
+    public BalanceException (String message, Throwable cause) {
+        super (message, cause);
+    }
+}
